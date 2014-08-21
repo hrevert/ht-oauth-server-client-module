c53a804f5c8f7e0551680a692819b265ddcba816
Ujjwal Ojha <bring2uo@gmail.com>
2014-08-21 10:58:13 +0545

    Update grant service name

--- config/module.config.php
+++ config/module.config.php
@@ -4,7 +4,7 @@ return [
     'zfr_oauth2_server' => [
         'grant_manager' => [
             'factories' => [
-                'oauth2_client' => 'HtOauth\Server\ClientModule\Factory\Oauth2ClientGrantFactory',
+                'HtOauth\Server\ClientModule\Grant\Oauth2Client' => 'HtOauth\Server\ClientModule\Factory\Oauth2ClientGrantFactory',
             ]
         ]
     ],
